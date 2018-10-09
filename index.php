4ef80b2065d30f711af414eda23db5bf946af669
Matěj <vas@email.com>
2018-10-09 10:52:21 +0200

    zaciname

---
+++ index.php
@@ -0,0 +1,15 @@
+
+<!DOCTYPE html>
+<html lang="cs" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>kok</title>
+  </head>
+  <body>
+    <p> Pohlaví :  <input type="radio" name="gen"> Muž  <input type="radio" name="gen"> Žena </p>
+    <p> Věk : <input type="number" name="age">
+      <br />
+      <br />
+      <input type="submit" name="sub" > Odeslat
+    </body>
+  </html>
